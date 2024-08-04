dbad434dcf8efe06d7f83e846660b8a353278d99
mmtmn <thiagomnobrega@gmail.com>
2024-08-04 12:16:45 -0300

    Improving it

--- artificialNeuron.py
+++ artificialNeuron.py
@@ -1,67 +1,158 @@
 import numpy as np
 
-class EnhancedNeuron:
+class DetailedIonChannel:
+    def __init__(self, type, conductance, reversal_potential):
+        self.type = type
+        self.conductance = conductance
+        self.reversal_potential = reversal_potential
+        self.state = np.random.rand()
+
+    def update_state(self, voltage):
+        self.state = 1 / (1 + np.exp(-(voltage - self.reversal_potential)))
+
+    def get_current(self, voltage):
+        return self.conductance * self.state * (voltage - self.reversal_potential)
+
+
+class DetailedNeuron:
     def __init__(self, num_inputs):
         self.num_inputs = num_inputs
-        self.weights_exc = np.random.rand(num_inputs)  # Excitatory weights
-        self.weights_inh = np.random.rand(num_inputs)  # Inhibitory weights
+        self.weights_exc = np.random.rand(num_inputs)
+        self.weights_inh = np.random.rand(num_inputs)
         self.bias = np.random.rand(1)
         self.last_spike_time = -np.inf
         self.refractory_period = 2.0
         self.learning_rate = 0.01
         self.spike_threshold = 0.5
-        self.dendritic_structure = np.random.rand(num_inputs)  # Non-linear summation factors
-    
+        self.dendritic_structure = np.random.rand(num_inputs)
+        
+        self.sodium_channel = DetailedIonChannel('Na', conductance=120, reversal_potential=50)
+        self.potassium_channel = DetailedIonChannel('K', conductance=36, reversal_potential=-77)
+        self.calcium_channel = DetailedIonChannel('Ca', conductance=2, reversal_potential=120)
+        
+        self.membrane_potential = -70
+        self.threshold_potential = -55
+        self.reset_potential = -70
+        self.membrane_resistance = 10
+        self.membrane_capacitance = 1
+        self.time_step = 1
+        
+        self.ltp_factor = 0.01
+        self.ltd_factor = 0.01
+
+        self.astrocyte_support = 0.1
+        self.dopamine_level = 1.0
+
+        self.axon_length = np.random.rand()
+        self.dendritic_tree = np.random.rand(num_inputs)
+
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
-    
-    def synaptic_plasticity(self, inputs, output):
-        # Hebbian learning
-        self.weights_exc += self.learning_rate * inputs * output
-        self.weights_inh -= self.learning_rate * inputs * output  # Inhibitory synapses learn inversely
+
+    def synaptic_plasticity(self, inputs, output, delta_t):
+        if output > self.spike_threshold:
+            self.weights_exc += self.ltp_factor * inputs * output
+            self.weights_inh -= self.ltp_factor * inputs * output
+        else:
+            self.weights_exc -= self.ltd_factor * inputs * (1 - output)
+            self.weights_inh += self.ltd_factor * inputs * (1 - output)
 
     def stdp(self, delta_t):
-        # STDP learning rule
-        tau = 20.0  # Time constant
+        tau = 20.0
         if delta_t > 0:
             return np.exp(-delta_t / tau)
         else:
             return -np.exp(delta_t / tau)
-    
+
+    def update_ion_channels(self):
+        self.sodium_channel.update_state(self.membrane_potential)
+        self.potassium_channel.update_state(self.membrane_potential)
+        self.calcium_channel.update_state(self.membrane_potential)
+
+    def calculate_total_current(self):
+        sodium_current = self.sodium_channel.get_current(self.membrane_potential)
+        potassium_current = self.potassium_channel.get_current(self.membrane_potential)
+        calcium_current = self.calcium_channel.get_current(self.membrane_potential)
+        return sodium_current + potassium_current + calcium_current
+
+    def ion_channel_dynamics(self):
+        total_current = self.calculate_total_current()
+        delta_v = (total_current / self.membrane_resistance) * self.time_step
+        self.membrane_potential += delta_v
+
+        if self.membrane_potential >= self.threshold_potential:
+            self.membrane_potential = self.reset_potential
+            return 1
+        else:
+            return 0
+
+    def synaptic_transmission(self, neurotransmitter_release):
+        receptor_binding = neurotransmitter_release * np.random.rand()
+        return receptor_binding
+
+    def intracellular_signaling(self, inputs):
+        signaling = np.sum(inputs * self.weights_exc) * np.random.rand()
+        return signaling
+
+    def detailed_dendritic_processing(self, inputs):
+        processed_inputs = inputs * self.dendritic_tree
+        return np.sum(processed_inputs)
+
     def forward(self, inputs, current_time):
         if current_time - self.last_spike_time < self.refractory_period:
             return 0
+
+        inputs += self.astrocyte_support
+        inputs *= self.dopamine_level
+
+        processed_inputs = self.detailed_dendritic_processing(inputs)
+
+        self.update_ion_channels()
+        output = self.ion_channel_dynamics()
         
-        # Separate excitatory and inhibitory inputs
-        exc_inputs = inputs * (inputs > 0)
-        inh_inputs = inputs * (inputs <= 0)
-        
-        # Non-linear summation for dendritic inputs
-        dendritic_exc_sum = np.sum(self.dendritic_structure * exc_inputs ** 2)
-        dendritic_inh_sum = np.sum(self.dendritic_structure * inh_inputs ** 2)
-        
-        # Compute weighted sum
-        weighted_sum = np.dot(exc_inputs, self.weights_exc) - np.dot(inh_inputs, self.weights_inh) + dendritic_exc_sum - dendritic_inh_sum + self.bias
-        output = self.sigmoid(weighted_sum)
-        
-        # Check for neuron firing
         if output > self.spike_threshold:
             delta_t = current_time - self.last_spike_time
             self.last_spike_time = current_time
-            self.synaptic_plasticity(inputs, output)
+            self.synaptic_plasticity(processed_inputs, output, delta_t)
             
-            # Apply STDP
             for i in range(self.num_inputs):
-                self.weights_exc[i] += self.learning_rate * self.stdp(delta_t) * exc_inputs[i]
-                self.weights_inh[i] += self.learning_rate * self.stdp(delta_t) * inh_inputs[i]
+                self.weights_exc[i] += self.learning_rate * self.stdp(delta_t) * processed_inputs
+                self.weights_inh[i] += self.learning_rate * self.stdp(delta_t) * processed_inputs
         
-        return output
+        neurotransmitter_release = np.random.rand()
+        receptor_binding = self.synaptic_transmission(neurotransmitter_release)
+        signaling = self.intracellular_signaling(processed_inputs)
+        
+        return output, receptor_binding, signaling
+
+class NeuralNetwork:
+    def __init__(self, num_neurons, num_inputs):
+        self.neurons = [DetailedNeuron(num_inputs) for _ in range(num_neurons)]
+
+    def forward(self, inputs, current_time):
+        outputs = []
+        for neuron in self.neurons:
+            output, receptor_binding, signaling = neuron.forward(inputs, current_time)
+            outputs.append((output, receptor_binding, signaling))
+        return outputs
+
+    def train(self, inputs, targets, epochs):
+        for epoch in range(epochs):
+            for i in range(len(inputs)):
+                current_time = epoch * len(inputs) + i
+                outputs = self.forward(inputs[i], current_time)
+                # Placeholder for training logic to adjust neuron weights based on error
+                # e.g., backpropagation or other learning rules
 
 # Example usage
-inputs = np.array([0.5, -0.3, 0.2, -0.1])  # Example inputs with both excitatory and inhibitory signals
-neuron = EnhancedNeuron(num_inputs=len(inputs))
+num_neurons = 10
+num_inputs = 5
+network = NeuralNetwork(num_neurons, num_inputs)
 
-# Simulate over time
+inputs = np.random.rand(num_inputs)
+targets = np.random.rand(num_neurons)
+network.train([inputs], [targets], epochs=10)
+
 for t in range(10):
-    output = neuron.forward(inputs, current_time=t)
-    print(f"Time {t}: Neuron output: {output}")
+    outputs = network.forward(inputs, current_time=t)
+    print(f"Time {t}: Network outputs: {outputs}")
